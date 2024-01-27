62d67f88acf2cb319e2e4a77b1263acd6f15eba4
Pankaj Kumar <pankaj.k@zohocorp.com>
2024-01-27 18:08:52 +0530

    Start of node js

---
+++ test.js
@@ -0,0 +1,11 @@
+console.log("I'm Pankaj");
+
+function satHello(){
+    console.log("Hello from Pankaj.");
+}
+
+satHello();
+
+// console.log(window);  // there is no window object in nodejs
+
+console.log(global);  // we have global instead of window in nodejs
